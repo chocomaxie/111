fda389c0d2134d1d489d1736b9a873eb9b8a4660
choy <choy@choys-MBP.bbrouter>
2025-10-26 15:25:01 +0800

    pakening

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,10 +1,20 @@
+// resources/js/Pages/Home.jsx
+
 import React from 'react';
-import { Inertia } from '@inertiajs/inertia';
+// 🚨 TINANGGAL: import { Inertia } from '@inertiajs/inertia';
+
+// Kung gagamitin mo ang router functions (tulad ng visit, reload, etc.),
+// gamitin ang bagong import:
+// import { router } from '@inertiajs/react';
 
 export default function Home(props) {
+    // Kung gusto mong i-check ang status ng Inertia:
+    // const { component, props } = router.page;
+
     return (
         <div>
             <h1>{props.message}</h1>
+            {/* Optional: <button onClick={() => router.visit('/another-page')}>Go</button> */}
         </div>
     );
 }
