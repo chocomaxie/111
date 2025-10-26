411e5f7b223f89ff1f4da5dabfbd44558eb7cb30
choy <choy@choys-MBP.bbrouter>
2025-10-26 15:18:56 +0800

    pak

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,13 +1,22 @@
+// resources/js/app.jsx
+
 import React from 'react';
+import './bootstrap';
+import '../css/app.css';
+
+// 🚨 BAGO: createInertiaApp ang gamitin, hindi App component
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+
+createInertiaApp({
+    // Siguraduhin na ang title ay nasa inyong pages
+    title: (title) => title ? `${title} - Your App Name` : 'Your App Name',
+
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+
+    setup({ el, App, props }) {
+        return createRoot(el).render(<App {...props} />);
+    },
+});
 
 import { createRoot } from 'react-dom/client';
-import { App } from '@inertiajs/inertia-react';
-
-const el = document.getElementById('app');
-
-createRoot(el).render(
-  <App
-    initialPage={JSON.parse(el.dataset.page)}
-    resolveComponent={(name) => import(`./Pages/${name}`).then(module => module.default)}
-  />
-);
