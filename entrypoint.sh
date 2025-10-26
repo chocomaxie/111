01a7a0c3b2f89b572c62f5c93a9622983316cd0a
choy <choy@choys-MBP.bbrouter>
2025-10-26 14:46:28 +0800

    Dasdsadsadasda

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# --- PERMISSIONS FIX (Dagdag na layer ng security para sa write access) ---
+# Run Laravel startup commands only at runtime
+# Tiyakin ang tamang permission bago mag-run ng Artisan commands
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
-# -------------------------------------------------------------------------
 
-# Run Laravel startup commands only at runtime, when ENV variables are available
 php artisan key:generate
 php artisan config:clear
 php artisan package:discover --ansi
